2121e5fed9e763730c9c4744015b65317ab2d19b
tanay27 <tanaykarmarkar@gmail.com>
2018-07-04 12:48:14 -0400

    change.

---
+++ Demo.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("Let Machine Talk")
+
+
+# [Section](#Section_ID)
+
+# # Tite
+# ## Subtitle
+# ### Heading
+# 
+# $X+2=4$
+# 
+# _jsufhds_
+
+# > the above is the equation to demonstrate symbol
+# > 
+# this is the change of line but it won't change because it's not over
+
+# <img src="img.jpg"></img>
+
+# ***
+
+# <a id="Section_ID"></a>
+
+# _[github](https://github.com/tanay27)_
+
+# In[ ]:
+
+print("hello")
+
+
+# In[4]:
+
+print("Earth")
+
+
+# In[ ]:
+
+
+
